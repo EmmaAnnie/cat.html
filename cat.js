f8f6d4b94e827b7a0de7f0bd570891dc397b45f4
EmmaAnnie <emmabuchta06@gmail.com>
2025-03-11 11:41:30 -0600

    Update cat.js

--- cat.js
+++ cat.js
@@ -4,7 +4,6 @@ fact.textContent = "Loading..."
 fetch("https://catfact.ninja/fact")
 .then(response = response.json())
 .then(data => (
-    console.log(data);
     console.log(data.fact);
     fact.textContent = data.fact;
 ))
