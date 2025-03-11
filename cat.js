a6fafdec776402d8688eab050359c827dbb28a1b
EmmaAnnie <emmabuchta06@gmail.com>
2025-03-11 11:50:44 -0600

    Update cat.js

--- cat.js
+++ cat.js
@@ -3,7 +3,14 @@ fact.textContent = "Loading..."
 
 fetch("https://catfact.ninja/fact")
 .then(response = response.json())
-.then(data => (
+.then(data => {
+    console.log(data)
     console.log(data.fact);
     fact.textContent = data.fact;
-))
+});
+
+fetch("https://cataas.com/cat?type=medium&json=true")
+.then(response => response.json())
+.then(data=>{
+    console.log(data.url);
+})
